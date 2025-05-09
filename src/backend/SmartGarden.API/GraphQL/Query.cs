59ff962f75c659f464f140f5bb83dcbe18825a1c
Michael Froschauer <mi.froschauer@gmx.at>
2025-05-09 13:08:36 +0200

    Add graphQL query class

---
+++ src/backend/SmartGarden.API/GraphQL/Query.cs
@@ -0,0 +1,11 @@
+using SmartGarden.API.Dtos.Actuator;
+using SmartGarden.EntityFramework;
+using SmartGarden.EntityFramework.Models;
+
+namespace SmartGarden.API.GraphQL;
+
+// https://localhost:5002/graphql/
+// https://chillicream.com/docs/hotchocolate/v15/defining-a-schema/queries
+// https://chillicream.com/docs/hotchocolate/v11/integrations/entity-framework
+
+public class Query { }
